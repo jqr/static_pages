c8d5e4e2ed4bcccfe315340a54b84ea4d180e4b4
Elijah Miller <elijah.miller@gmail.com>
2009-04-25 10:41:54 -0400

    Keeping render_optional_error_file a private method.

--- lib/static_pages.rb
+++ lib/static_pages.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  private
+  
   def render_optional_error_file(status)
     begin
       code = ActionController::StatusCodes::SYMBOL_TO_STATUS_CODE[status]
