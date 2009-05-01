5577a1a8c0fd72233144856bfee4d025c981bf00
Elijah Miller <elijah.miller@gmail.com>
2009-04-30 20:24:45 -0400

    Application routes take precedence over static_pages routes [1].
    [1] http://giantrobots.thoughtbot.com/2009/4/23/tips-for-writing-your-own-rails-engine

--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect ':action', :controller => 'static_pages'
-end
--- lib/static_pages.rb
+++ lib/static_pages.rb
@@ -10,3 +10,16 @@ module StaticPages
     end
   end
 end
+
+class ActionController::Routing::RouteSet
+  def load_routes_with_static_pages!
+    static_pages_routes = File.join(File.dirname(__FILE__), 
+                       *%w[.. config static_pages_routes.rb])
+    unless configuration_files.include? static_pages_routes
+      add_configuration_file(static_pages_routes)
+    end
+    load_routes_without_static_pages!
+  end
+
+  alias_method_chain :load_routes!, :static_pages
+end
