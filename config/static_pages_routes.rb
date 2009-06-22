0bc0b1dfca4deeb2905e56939a8fa4822904f579
Elijah Miller <elijah.miller@gmail.com>
2009-06-22 15:43:28 -0400

    Naming the static page route so you can use the path/url helpers: static_page_path('privacy')

--- config/static_pages_routes.rb
+++ config/static_pages_routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'static_pages'
-  map.connect ':action/:id', :controller => 'static_pages'
+  map.static_page ':action/:id', :controller => 'static_pages'
 end
