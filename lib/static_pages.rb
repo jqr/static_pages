b5bd4259ad4979c8668c36f67b00a407a1f5b8f9
Elijah Miller <elijah.miller@gmail.com>
2009-08-27 09:54:50 -0400

    Returning the proper status code for error pages.

--- lib/static_pages.rb
+++ lib/static_pages.rb
@@ -4,7 +4,7 @@ module StaticPages
   def render_optional_error_file(status)
     begin
       code = ActionController::StatusCodes::SYMBOL_TO_STATUS_CODE[status]
-      render :template => "/static_pages/#{code}"
+      render :status => status, :template => "/static_pages/#{code}"
     rescue
       super
     end
